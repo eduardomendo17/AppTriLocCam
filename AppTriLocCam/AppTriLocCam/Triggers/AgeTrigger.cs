6fc4d6e1ff7b579fbad064dce9a1da2c74a5f164
Eduardo Mendoza <eduardomendo17@hotmail.com>
2022-03-16 10:04:44 -0600

    Agregar archivos de proyecto.

---
+++ AppTriLocCam/AppTriLocCam/Triggers/AgeTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppTriLocCam.Triggers
+{
+    public class AgeTrigger : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry sender)
+        {
+            int n;
+            bool isNumeric = int.TryParse(sender.Text, out n);
+            if (!isNumeric)
+            {
+                // No es numérico
+                sender.Text = "";
+            }
+            else
+            {
+                // Es numérico
+                if (n < 0)
+                {
+                    // Es menor que cero regresamos cero
+                    sender.Text = "0";
+                }
+                else if (n > 99)
+                {
+                    // Si es mayor que 99 regresamos 99
+                    sender.Text = "99";
+                }
+            }
+        }
+    }
+}
---
+++ AppTriLocCam/AppTriLocCam/ViewModels/LocationViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace AppTriLocCam.ViewModels
+{
+    public class LocationViewModel : BaseViewModel
+    {
+        // Commandos
+        Command _GetLocationCommand;
+        public Command GetLocationCommand => _GetLocationCommand ?? (_GetLocationCommand = new Command(GetLocationAction));
+
+        // Propiedades
+        private double _latitude;
+        public double Latitude
+        {
+            get => _latitude;
+            set => SetProperty(ref _latitude, value);
+        }
+
+        private double _longitude;
+        public double Longitude
+        {
+            get => _longitude;
+            set => SetProperty(ref _longitude, value);
+        }
+
+        // Constructor
+        public LocationViewModel()
+        { }
+
+        // Métodos
+        private async void GetLocationAction()
+        {
+            try
+            {
+                var location = await Geolocation.GetLastKnownLocationAsync();
+                if (location != null)
+                {
+                    Latitude = location.Latitude;
+                    Longitude = location.Longitude;
+
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+        }
+    }
+}
